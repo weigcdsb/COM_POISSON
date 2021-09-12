2e1d7081dd96aa254f2d72cf5c0e2d219a7de66f
Ganchao Wei <ganchao.wei@uconn.edu>
2021-09-12 15:22:56 -0400

    transfer to run test grid llhd

--- demo/smoother_fail/testLlhdGrid.m
+++ demo/smoother_fail/testLlhdGrid.m
@@ -6,12 +6,20 @@ clear all; close all; clc;
 rng(1)
 
 betaStart = 0; 
-gamStart = -1;
+% gamStart = -1;
 
-nGrid_nu = 10;
+% nGrid_nu = 10;
+% nGrid_lam = 10;
+% betaRange = linspace(0,3,nGrid_lam);
+% gamRange = linspace(1,3,nGrid_nu);
+
+% nu constant:
+gamStart = .5;
 nGrid_lam = 10;
-betaRange = linspace(0,3,nGrid_lam);
-gamRange = linspace(1,3,nGrid_nu);
+nGrid_nu = 1;
+betaRange = linspace(0,4,nGrid_lam);
+gamRange = linspace(0,0,nGrid_nu);
+
 
 testLlhd_filt_exact = zeros(nGrid_nu, nGrid_lam);
 testLlhd_filt = zeros(nGrid_nu, nGrid_lam);
@@ -36,18 +44,27 @@ testLlhd_newton = zeros(nGrid_nu, nGrid_lam);
 % windSize select by forward chaining
 windSize_opt = zeros(nGrid_nu, nGrid_lam);
 
-for i = 1:nGrid_lam
-    for j = 1:nGrid_nu
-        [llhd_mean_tmp, windSize_opt(j,i)]= testLlhdCalc_linear_CV(betaStart,betaStart + betaRange(i),...
-            gamStart,gamStart + gamRange(j),200,10);
-        
-        testLlhd_filt_exact(j,i) = llhd_mean_tmp(1);
-        testLlhd_filt(j,i) = llhd_mean_tmp(2);
-        testLlhd_filt_wind(j,i) = llhd_mean_tmp(3);
-        testLlhd_newton(j,i) = llhd_mean_tmp(4);
-        
+nRep = 100;
+for k = 1:nRep
+    for i = 1:nGrid_lam
+        for j = 1:nGrid_nu
+            [llhd_mean_tmp, windSize_tmp]= testLlhdCalc_linear_CV(betaStart,betaStart + betaRange(i),...
+                gamStart,gamStart + gamRange(j),200,1);
+            
+            windSize_opt(j,i) = windSize_opt(j,i) + windSize_tmp;
+            testLlhd_filt_exact(j,i) = testLlhd_filt_exact(j,i) + llhd_mean_tmp(1);
+            testLlhd_filt(j,i) = testLlhd_filt(j,i) + llhd_mean_tmp(2);
+            testLlhd_filt_wind(j,i) = testLlhd_filt_wind(j,i) + llhd_mean_tmp(3);
+            testLlhd_newton(j,i) = testLlhd_newton(j,i) + llhd_mean_tmp(4);
+        end
     end
 end
+
+windSize_opt = windSize_opt/nRep;
+testLlhd_filt_exact = testLlhd_filt_exact/nRep;
+testLlhd_filt = testLlhd_filt/nRep;
+testLlhd_filt_wind = testLlhd_filt_wind/nRep;
+testLlhd_newton = testLlhd_newton/nRep;
 
 %% plot grid of llhd: constant
 % clear all; close all; clc;
@@ -89,7 +106,7 @@ cLim_all = [min([testLlhd_filt_exact(:) - testLlhd_filt(:);...
 figure;
 subplot(1,3,1)
 imagesc(betaRange, gamRange,testLlhd_filt_exact - testLlhd_filt)
-ylabel('range of \gamma')
+ylabel("range of \gamma, start from " + gamStart)
 title('exact - fisher')
 colorbar()
 set(gca,'CLim',cLim_all)
@@ -101,7 +118,7 @@ colorbar()
 set(gca,'CLim',cLim_all)
 subplot(1,3,3)
 imagesc(betaRange, gamRange,testLlhd_newton - testLlhd_filt)
-xlabel('range of \beta')
+xlabel("range of \beta, start from" + betaStart)
 title('newton - fisher')
 colorbar()
 set(gca,'CLim',cLim_all)
@@ -110,7 +127,7 @@ set(gca,'CLim',cLim_all)
 figure;
 subplot(1,3,1)
 imagesc(betaRange, gamRange,testLlhd_filt_exact - testLlhd_filt)
-ylabel('range of \gamma')
+ylabel("range of \gamma, start from " + gamStart)
 title('exact - fisher')
 colorbar()
 subplot(1,3,2)
@@ -120,7 +137,7 @@ title('window - fisher')
 colorbar()
 subplot(1,3,3)
 imagesc(betaRange, gamRange,testLlhd_newton - testLlhd_filt)
-xlabel('range of \beta')
+xlabel("range of \beta, start from" + betaStart)
 title('newton - fisher')
 colorbar()
 
@@ -129,19 +146,19 @@ figure;
 subplot(1,2,1)
 imagesc(betaRange, gamRange,(testLlhd_newton - testLlhd_filt_wind)>0)
 title('newton - window: >0 ?')
-ylabel('range of \gamma')
+ylabel("range of \gamma, start from " + gamStart)
 colorbar()
 subplot(1,2,2)
 imagesc(betaRange, gamRange,testLlhd_newton - testLlhd_filt_wind)
 % title('newton - window 5')
 title('newton - window: value')
-xlabel('range of \beta')
+xlabel("range of \beta, start from" + betaStart)
 colorbar()
 
 % selected window size
 figure;
 imagesc(betaRange, gamRange,windSize_opt)
 title('selected window size')
-ylabel('range of \gamma')
-xlabel('range of \beta')
+ylabel("range of \gamma, start from " + gamStart)
+xlabel("range of \beta, start from" + betaStart)
 colorbar()
