61d5ce2276227b753e16edfb98b9b5342bca7d99
Ganchao Wei <60019090+weigcdsb@users.noreply.github.com>
2021-10-21 18:27:43 -0400

    constantNu + all74

--- demo/v1/theta_interp1.m
+++ demo/v1/theta_interp1.m
@@ -4,6 +4,9 @@ theta_ho = zeros(size(theta_tr,1), length(hoIdx));
 for d = 1:size(theta_tr,1)
     theta_ho(d,:) = interp1(trIdx,theta_tr(d,:),hoIdx);
 end
-if(isnan(theta_ho(:,end))); theta_ho(:,end) = theta_tr(:,end);end
-
+if(sum(isnan(theta_ho(:,end))) > 0); theta_ho(:,end) = theta_tr(:,end);end
+if(sum(isnan(theta_ho(:,1))) > 0); theta_ho(:,1) = theta_tr(:,1);end
 end
+
+
+
